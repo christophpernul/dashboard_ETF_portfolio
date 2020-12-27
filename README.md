96b0e5f35fb3e602c735ad555dc9ea15044435e7
Christoph Pernul <cpernul@gmail.com>
2020-12-27 01:10:40 +0100

    Different column heights in a given row.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dashboard_ETF_portfolio
 A simple dashboard, that allows a basic analysis of a monthly ETF (exchange traded funds) savings plan. Uses python dash.
+Necessary libraries:
+- odfpy: Read in LibreOffice documents
--- dashboard_app.py
+++ dashboard_app.py
@@ -1,92 +1,71 @@
-import pandas as pd
+# import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 # from dash.dependencies import Input, Output
+import portfolio_lib as pl
 
 colors = {
     'background': '#000000',
     'text': '#7FDBFF'
 }
 
+################################ Data Processing for ETF portfolio #####################################################
+(df_etf_init, df_orders_init, _, _) = pl.load_data()
+(df_orders, _) = pl.preprocess_orders(df_orders_init)
+df_etf = pl.preprocess_etf_masterdata(df_etf_init)
+orders_etf = pl.enrich_orders(df_orders, df_etf)
+portfolio = pl.get_current_portfolio(orders_etf)
+
+unique_etfs = orders_etf[["ISIN", "Name"]].drop_duplicates().sort_values("ISIN").reset_index(drop=True)
+
+### ------------------- Portfolio of monthly savings plan ------------------
+group_cols = ["Region", "Type"]
+compute_cols = ["Betrag", "Betrag"]
+agg_functions = ["sum", "sum"]
+grouped_portfolio = pl.compute_percentage_per_group(portfolio, group_cols, compute_cols, agg_functions)
+
+
+################################ Define Dash App configuration ### #####################################################
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 body = html.Div([
                     html.H1(children="Bootstrap Responsive Dashboard Example"),
-                    html.Div(dbc.Alert("A single row")),
-                    html.H2("Columns with no_gutters = False"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=4),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=4),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=4)
-                        ]
-                    ),
-                    html.H2("Columns with no_gutters = True"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=4),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=4),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=4)
-                        ],
-                        no_gutters=True
-                    ),
-                    html.H2("Columns with justify = start"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=2)
-                        ],
-                        justify='start'
-                    ),
-                    html.H2("Columns with justify = end"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=2)
-                        ],
-                        justify='end'
-                    ),
-                    html.H2("Columns with justify = center"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=2)
-                        ],
-                        justify='center'
-                    ),
-                    html.H2("Columns with justify = between"),
-                    dbc.Row(
-                        [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=2)
-                        ],
-                        justify='between'
-                    ),
+
                     html.H2("Columns with justify = around"),
                     dbc.Row(
                         [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), width=2),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=2)
+                            dbc.Col(html.Div(dbc.Alert("Column 1")), width=6),
+                            dbc.Col(html.Div(dbc.Alert("Column 3")), width=6)
                         ],
                         justify='around'
                     ),
-                    html.H2("Specify widths for different size-devices:"),
+                    html.H2("Columns with rows in it"),
                     dbc.Row(
                         [
-                            dbc.Col(html.Div(dbc.Alert("Column 1")), lg=4, md=6, xs=12),
-                            dbc.Col(html.Div(dbc.Alert("Column 2")), lg=4, md=6, xs=12),
-                            dbc.Col(html.Div(dbc.Alert("Column 3")), lg=4, md=6, xs=12)
+                            dbc.Col(html.Div([html.Div("Text"),html.Br(), html.Div("Text"),
+                                              html.Br(), html.Div("Text"),html.Br(), html.Div("Text"),
+                                              html.Br(), html.Div("Text"),html.Br(), html.Div("Text")]), width=6),
+                            dbc.Col([
+                                    dbc.Row([
+                                        dbc.Col(html.Div([html.Div("Text"),html.Br(), html.Div("Text"),
+                                              html.Br(), html.Div("Text"),html.Br(), html.Div("Text")])
+                                            , width=6),
+                                        dbc.Col(html.Div(dbc.Alert("Column 2")), width=6)
+                                    ]),
+                                    dbc.Row([
+                                        dbc.Col(html.Div(dbc.Alert("Column 1")), width=6),
+                                        dbc.Col(html.Div(dbc.Alert("Column 2")), width=6)
+                                    ])
+                                ],
+                                width=6
+                            )
                         ],
-                        justify='around'
-                    )
+                        justify='start'
+                    ),
+                html.Div(dbc.Alert("Next row!"))
                 ]
             )
 
